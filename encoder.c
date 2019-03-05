84fff8270653cb3d6e07508630482ea4b60cd255
yuvitz <44752193+yuvitz@users.noreply.github.com>
2019-03-05 16:48:17 +0200

    Add files via upload

--- encoder.c
+++ encoder.c
@@ -4,18 +4,38 @@
 
 int main(int argc, char **argv)
 {
-	int j, i;
-	int deb_mode=0;
-	int c;
+	//mode params
+	int deb_mode=0, enc_mode=0, addition=0; /*read_mode=0;*/
 
-	//check debug mode
-	for (i = 0; i < argc; i++){
+	//index params
+	int j, i, enc_arg_index, enc_char_index=2; /*read_arg_index;*/
+	//read char
+	int c;
+	//File *file;
+
+	//check mode
+	for (i = 1; i < argc; i++){
 		if(strcmp(argv[i], "-D")==0)
 			if(deb_mode==0){
 				deb_mode=1;
 				for (j = 1; j < argc; j++)
 					puts(argv[j]);	
 			}
+		//set addition
+		if((strncmp(argv[i], "+e", 2))==0){
+			enc_mode=1;
+			addition=1;
+			enc_arg_index=i;	
+		}
+		//set substruction
+		if((strncmp(argv[i], "-e", 2))==0){
+			enc_mode=1;
+			enc_arg_index=i;
+		}
+		/*if(strncmp(argv[i], "-i", 2)==0){
+			read_mode=1;
+			read_arg_index=i;		
+		}	*/	
 	}
 
 //read
@@ -23,17 +43,42 @@ int main(int argc, char **argv)
 
 	 while(c!=EOF){
 	 	if(deb_mode==1)
-	 			fprintf(stderr, "0x%x\t", c);
-	 	if ((c>64) & (c<91)){
- 			if(deb_mode==1)
-	 			fprintf(stderr, "0x%x\n", c+32);
-	 		putc(c+32, stdout);
-	 	}
-	 	else{
-	 		if(deb_mode==1)
-	 			fprintf(stderr, "0x%x\n", c);
-	 		putc(c, stdout);
-	 	}
-	 c=fgetc(stdin);
+	 		fprintf(stderr, "%#02x\t", c);
+		//encryption mode
+		if(enc_mode==1){
+			//addition
+			if(addition==1){
+				fputc(c+argv[enc_arg_index][enc_char_index], stdout);
+				if(deb_mode==1)
+			 		fprintf(stderr, "%#02x\n", c+argv[enc_arg_index][enc_char_index]);				
+			}
+			//substruction
+			else{
+				fputc(c-argv[enc_arg_index][enc_char_index], stdout);
+				if(deb_mode==1)
+			 		fprintf(stderr, "%#02x\n", c-argv[enc_arg_index][enc_char_index]);	
+			}
+			enc_char_index++;	
+			if (enc_char_index>strlen(argv[enc_arg_index])-1)
+		                enc_char_index=2;
+		        if(c =='\n'){
+                		enc_char_index=2;
+                		fputc('\n', stdout);
+            		}
+		}
+		//not encryption mode
+		else{
+	 		if ((c>='A') & (c<='Z')){
+ 				if(deb_mode==1)
+	 				fprintf(stderr, "%#02x\n", c+('a'-'A'));
+	 			putc(c+('a'-'A'), stdout);
+	 		}
+	 		else{
+	 			if(deb_mode==1)
+	 				fprintf(stderr, "%#02x\n", c);
+	 			putc(c, stdout);
+	 		}
+		}
+	 	c=fgetc(stdin);
 	}
 }
