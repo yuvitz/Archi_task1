9df2f871c1de03814922c6b12fcb5c25aef28ba8
yuvitz <44752193+yuvitz@users.noreply.github.com>
2019-03-05 11:32:09 +0200

    Add files via upload

---
+++ encoder.c
@@ -0,0 +1,38 @@
+#include<unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+	int j, i;
+	int deb_mode=0;
+	int c;
+
+	//check debug mode
+	for (i = 0; i < argc; i++){
+		if(strcmp(argv[i], "-D")==0)
+			if(deb_mode==0){
+				deb_mode=1;
+				for (j = 1; j < argc; j++)
+					puts(argv[j]);	
+			}
+	}
+
+//read
+	c=fgetc(stdin);
+
+	 while(c!=EOF){
+	 	if ((c>64) & (c<91)){
+ 			if(deb_mode==1)
+	 			fprintf(stderr, "0x%x\n", c);
+	 		putc(c+32, stdout);
+	 	}
+	 	else{
+	 		if(deb_mode==1)
+	 			fprintf(stderr, "0x%x\n", c);
+	 		putc(c, stdout);
+	 	}
+	 c=fgetc(stdin);
+	}
+//   return 0;
+}
